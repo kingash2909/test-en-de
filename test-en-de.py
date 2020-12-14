78718efd22c4ec16d14be79fd5cdbda75ca6deca
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2020-12-14 20:45:17 +0530

    Update test-en-de.py

--- test-en-de.py
+++ test-en-de.py
@@ -10,6 +10,4 @@ def home():
     return "hello"
 
 if __name__ =='__main__':  
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port, debug = True)  
-   	 
+    app.run(debug = True)     	 
